c8659867ab67e9c377ad9478e457ef9ea4a695e3
viktorian <iamviktorian@gmail.com>
2017-12-13 00:39:04 +0200

    complete 1-4 sections

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
-/* ScrollBar */
-$('html').niceScroll();
-/* /// ScrollBar */
+$(document).ready(function(){
+
+    /* ScrollBar */
+    $('html').niceScroll();
+    /* /// ScrollBar */
+
+    /* Slick */
+    $('.slider_box_inner').slick({
+
+        dots: true
+        
+    });
+
+    $('.fake_next').on('click', function(){
+        $(this).parent().find('.slider_box_inner .slick-next').trigger('click');
+    });
+    $('.fake_prev').on('click', function(){
+        $(this).parent().find('.slider_box_inner .slick-prev').trigger('click');
+    });
+    /* /// Slick */
+
+});
